b4222e4d393001a3fd6a2ad0525e8db6edeeba81
Dnyaneshwari  Sonwane <dnyanusonwane2008@gmail.com>
2026-09-09 19:33:28 +0530

    Updated Day 1 DSA solution

--- Strings/LongestSubstring.py
+++ Strings/LongestSubstring.py
@@ -1,30 +1,22 @@
-def length_of_longest_substring(s):
-    seen = {}
-    left = 0
-    max_length = 0
+def longest_unique(s):
+    window = set()
+    start = 0
+    answer = 0
 
-    for right, char in enumerate(s):
+    for end in range(len(s)):
+        while s[end] in window:
+            window.remove(s[start])
+            start += 1
 
-        # If character is already in the current window
-        if char in seen and seen[char] >= left:
-            left = seen[char] + 1
+        window.add(s[end])
 
-        # Store the latest index of the character
-        seen[char] = right
+        if end - start + 1 > answer:
+            answer = end - start + 1
 
-        # Calculate current window length
-        current_length = right - left + 1
-
-        # Update maximum length
-        max_length = max(max_length, current_length)
-
-    return max_length
+    return answer
 
 
-# Example
-s = "abcabcbb"
+text = "abcabcbb"
 
-result = length_of_longest_substring(s)
-
-print("Input:", s)
-print("Longest substring length:", result)
+print("String:", text)
+print("Answer:", longest_unique(text))
